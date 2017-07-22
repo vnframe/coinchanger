b566c5e20d49f03a7213804ae0fff2d066128e2f
vnframe <vnframe95@icloud.com>
2017-07-22 16:34:01 -0400

    quarters working

--- coinchanger.rb
+++ coinchanger.rb
@@ -3,23 +3,16 @@ def coinchanger(change)
   if change.between?(1, 4)
     coin_values[:penny] = 1 * change
   elsif
-    change.between?(5, 10)
-    coin_values[:nickel] = 5 * (change/5)
+    change == 5
+    coin_values[:nickel] = 5
     p coin_values
-#   if coin_values 25
-#   coin_values[:quarter] = 25
-#   p coin_values
-# elsif
-#   coin_values => 10
-#   coin_values[:dime] = 10
-#   p coin_values
-# elsif
-#   coin_values => 5
-#   coin_values[:nickels] = 5
-#   p coin_values
-# else
-#   coin_values[:penny] = 1
-#    p coin_values
+  elsif
+    change.between?(10, 20)
+    coin_values[:dime] = 10 * (change/10)
+    p coin_values
+  else
+  coin_values[:quarter] = 25 * (change/25)
+  p coin_values
 
 end
 
--- coinchangertest.rb
+++ coinchangertest.rb
@@ -14,9 +14,25 @@ class Test_Coinchanger < Minitest::Test
   def test_change_nickel
     assert_equal({nickel: 5}, coinchanger(5))
   end
-  def test_two_nickels
-    assert_equal({nickel: 10}, coinchanger(10))
+  # def test_two_nickels
+  #   assert_equal({nickel: 10}, coinchanger(10))
+  # end
+  def test_change_dime
+    assert_equal({dime: 10}, coinchanger(10))
   end
+  def test_two_dimes
+    assert_equal({dime: 20}, coinchanger(20))
+  end
+  def test_change_quarter
+    assert_equal({quarter: 25}, coinchanger(25))
+  end
+  def test_one_dollar
+    assert_equal({quarter: 100}, coinchanger(100))
+  end
+
+  # def test_15
+  #   assert_equal({dime:10, nickel: 5}, coinchanger(15))
+  # end
   # def test_change_quarter
   #   assert_equal({quarter: 25}, coinchanger(25))
   # end
