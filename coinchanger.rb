8f34ecf69bcb606a17e02289e64263750b18be28
vnframe <vnframe95@icloud.com>
2017-07-24 12:49:48 -0400

    finished

--- coinchanger.rb
+++ coinchanger.rb
@@ -1,23 +1,42 @@
 def coinchanger(change)
-  coin_values = {}
-  if change.between?(1, 4)
-    coin_values[:penny] = 1 * change
-  elsif
-    change == 5
-    coin_values[:nickel] = 5
-    p coin_values
-  elsif
-    change.between?(10, 20)
-    coin_values[:dime] = 10 * (change/10)
-    p coin_values
-  else
-  coin_values[:quarter] = 25 * (change/25)
-  p coin_values
-
+  your_change = {quarter: 0, dime: 0, nickel: 0, penny: 0}
+  #will return coin values of change
+  coin_values = {quarter: 25, dime: 10, nickel: 5, penny: 1}
+  coin_values.each { |key, value| while value <= change
+   coin_values.values
+   your_change[key] += 1
+  change = change - value
+end}
+p your_change
 end
 
-  coin_values.each do |value, key|
-  coin_values.values
-  #coin_values.key
-  end
-end
+#   if change.between?(1, 4)
+#     coin_values[:penny] = 1 * change      #tried this first, changed direction
+#   elsif
+#     change == 5
+#     coin_values[:nickel] = 5
+#     p coin_values
+#   elsif
+#     change.between?(10, 20)
+#     coin_values[:dime] = 10 * (change/10)
+#     p coin_values
+#   else
+#   coin_values[:quarter] = 25 * (change/25) #sets coin value and multiplies coins
+#   p coin_values
+#
+# end
+# while (change > 25) do                    #changed direction again
+#     if change % 25 == 0
+#     coin_values[:quarter] = 25 * (change/25)
+#       p coin_values
+#   elsif   change % 10 == 0
+#     coin_values[:dime] = 10 * (change/10)
+#      p coin_values
+#   elsif
+#    change % 5 == 0
+#    coin_values[:nickel] = 5
+#    p coin_values
+#   else
+#     coin_values[:penny] = 1 * change
+#   end
+# end
--- coinchangertest.rb
+++ coinchangertest.rb
@@ -6,37 +6,37 @@ class Test_Coinchanger < Minitest::Test
   #   assert_equal({}, coinchanger())
   # end
   def test_change_one_penny
-    assert_equal({penny: 1}, coinchanger(1))
+    assert_equal({quarter: 0, dime: 0, nickel: 0, penny: 1}, coinchanger(1))
   end
   def test_change_pennies
-    assert_equal({penny: 4}, coinchanger(4))
+    assert_equal({quarter: 0, dime: 0, nickel: 0, penny: 4}, coinchanger(4))
   end
   def test_change_nickel
-    assert_equal({nickel: 5}, coinchanger(5))
+    assert_equal({quarter: 0, dime: 0, nickel: 1, penny: 0}, coinchanger(5))
   end
-  # def test_two_nickels
-  #   assert_equal({nickel: 10}, coinchanger(10))
-  # end
   def test_change_dime
-    assert_equal({dime: 10}, coinchanger(10))
+    assert_equal({quarter: 0, dime: 1, nickel: 0, penny: 0}, coinchanger(10))
   end
   def test_two_dimes
-    assert_equal({dime: 20}, coinchanger(20))
+    assert_equal({quarter: 0, dime: 2, nickel: 0, penny: 0}, coinchanger(20))
   end
   def test_change_quarter
-    assert_equal({quarter: 25}, coinchanger(25))
+    assert_equal({quarter: 1, dime: 0, nickel: 0, penny: 0}, coinchanger(25))
   end
   def test_one_dollar
-    assert_equal({quarter: 100}, coinchanger(100))
+    assert_equal({quarter: 4, dime: 0, nickel: 0, penny: 0}, coinchanger(100))
+  end
+  def test_quarter_and_dime
+    assert_equal({quarter: 1, dime: 0, nickel: 0, penny: 0}, coinchanger(25))
   end
 
-  # def test_15
-  #   assert_equal({dime:10, nickel: 5}, coinchanger(15))
-  # end
-  # def test_change_quarter
-  #   assert_equal({quarter: 25}, coinchanger(25))
-  # end
-  # def test_change_dime
-  #   assert_equal({dime: 10}, coinchanger(10))
-  # end
+  def test_15
+    assert_equal({quarter: 0, dime:1, nickel: 1, penny: 0}, coinchanger(15))
+  end
+  def test_change_quarter_and_dime
+    assert_equal({quarter: 1, dime: 1, nickel: 0, penny: 0}, coinchanger(35))
+  end
+  def test_change_over_dollar
+    assert_equal({quarter: 4, dime: 1, nickel: 1, penny: 3}, coinchanger(118))
+  end
 end
